7848fad12082838ef79aa88d291551cb0895ab00
AliweeLee <731275785@qq.com>
2017-04-20 18:20:49 +0800

    first change

--- zfc/recvCmd.cpp
+++ zfc/recvCmd.cpp
@@ -7,18 +7,27 @@ int map_x, map_y;//地图宽高
 int Qp, Qt;//警察和小偷数量
 int m, n;//视距
 int nround;//轮数
-struct coordinate
+typedef struct coordinate
 {
 	string id;
 	int x;
 	int y;
-	char move;
-};
+	char* move;
+}police, thief;
 struct bar
 {
 	int x;
 	int y;
 };
+vector<char*> vec_string;//INI或INF指令第一次分割
+vector<char*> vec_map;//地图大小
+vector<char*> vec_eye;//视距
+vector<char*> vec_num;//警察和小偷数量
+vector<char*> vec_coordinate;//坐标
+vector<coordinate> vec_police;//警察
+vector<coordinate> vec_thief;//小偷
+
+police a = { "0",0,0,"T" };
 
 //函数实现
 int recvIni(char *str)
@@ -84,5 +93,48 @@ void recvIniPol(char *str)
 void recvIniThi(char *str)
 {
 
+}
+
+void strtokCmd(char *str, vector<char*>& vec, const char *sep)
+{
+	char *p;
+	p = strtok(str, sep);
+	while (p)
+	{
+		vec.push_back(p);
+		p = strtok(NULL, sep);
+	}
+}
+
+void printvec(vector<char*>& vec)
+{
+	for (int i = 0; i<vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+}
+
+void transPOL(vector<char*>& vecin, vector<coordinate>& vecout)
+{
+	for (int i = 0, j = 0; i<vecin.size(); i = i + 3, j++)
+	{
+		vecout.at(j).id = vecin.at(i);
+		vecout.at(j).x = atoi(vecin.at(i + 1));
+		vecout.at(j).y = atoi(vecin.at(i + 2));
+	}
+}
+
+void initial()
+{
+	map_x = atoi(vec_map[0]);
+	map_y = atoi(vec_map[1]);
+	m = atoi(vec_eye[0]);
+	n = atoi(vec_eye[1]);
+	Qp = atoi(vec_num[0]);
+	Qt = atoi(vec_num[1]);
+	for (int i = 0; i<Qp; i++)
+	{
+		vec_police.push_back(a);
+	}
 }
 
--- zfc/recvCmd.h
+++ zfc/recvCmd.h
@@ -4,3 +4,9 @@ extern int recvCmd(char *str);
 extern int sendCmd(char *str);
 void recvIniPol(char *str);
 void recvIniThi(char *str);
+extern struct coordinate;
+
+void strtokCmd(char *str, vector<char*>& vec, const char *sep);
+void printvec(vector<char*>& vec);
+void transPOL(vector<char*>& vecin, vector<coordinate>& vecout);
+void initial();
